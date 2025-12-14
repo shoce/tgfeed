f415578b44fc7c76bf87a18f9b40b10351f11abc
shoce <yogze@icloud.com>
2025-12-14 11:59:45 +0700

    ॐ

--- tgfeed.go
+++ tgfeed.go
@@ -271,7 +271,7 @@ func FeedsCheck() error {
 func ts() string {
 	tnow := time.Now().In(time.FixedZone("IST", 330*60))
 	return fmt.Sprintf(
-		"%d%02d%02d:%02d%02d+",
+		"%d%02d%02d:%02d%02dॐ",
 		tnow.Year()%1000, tnow.Month(), tnow.Day(),
 		tnow.Hour(), tnow.Minute(),
 	)
